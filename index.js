f4ada100227c03fb995b46ec887401bfa2896f61
Jatin Malhan <jatinmalhan2300@.in>
2024-03-15 21:45:01 +0530

    Js OOps

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-const abc =1;
+ // this is procedural programming
+ 
+//  const user = {
+//     firstName:"Jatin",
+//     lastName:"Malhan",
+//     age:"24"
+// }
+
+//  function getAgeYear(age){
+//     return new Date().getFullYear()-age;
+//  }
+
+
+ // this is object oreinted programming 
+
+//  const user = {
+//         firstName:"Jatin",
+//         lastName:"Malhan",
+//         age:"24",
+//          getAgeYear : function(){
+//             return new Date().getFullYear()-user.age
+//          }
+//     }
+     
+
+//FACTORY FUNCTIONS
+ function createUser (firstName , lastName,age){
+    const user = {
+        firstName,
+        lastName,
+        age,
+        getAgeYear (){
+            return new Date().getFullYear()- user.age
+        },
+    }
+    return user
+    }
+ 
+const user1 = createUser('aman','raj',32)
+    
